d3d503795606410f031ec12841ec19b74371812e
Andrew Pike <59455297+AndrewPikeGIS@users.noreply.github.com>
2024-06-27 23:11:57 -0600

    added more log files. have a bug w position

--- log_processing.r
+++ log_processing.r
@@ -4,21 +4,12 @@ log_folder <- "data/team_logs"
 
 file_list <- list.files(log_folder)
 
-
-
-table_update <- create_clean_log_table(first_tbl)
-
-forwards_table <- create_forwards_table(table_update)
-
-goalie_table <- create_goalie_table(table_update)
-
 list_clean_log <- lapply(
     file_list,
     create_list_of_clean_logs,
     folder = log_folder
 )
 
-View(list_clean_log[[1]])
 
 list_forwards_tables <- lapply(list_clean_log, create_forwards_table)
 list_goalie_tables <- lapply(list_clean_log, create_goalie_table)
